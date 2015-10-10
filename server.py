8fe356f37631a8868168a2c3729a4a5b69386c11
Luis Capelo <luiscape@gmail.com>
2015-10-09 21:09:06 -0400

    fixing scheduler

--- server.py
+++ server.py
@@ -25,4 +25,23 @@ def Main():
 #
 # Starting schedule.
 #
-schedule.every(1).minute.do(Main)
+schedule.every(30).seconds.do(Main)
+
+def Main(verbose=True):
+  '''Wrapper to run all the scheduled tasks.'''
+
+  if verbose:
+    print '%s Running scheduler.' % item('prompt_bullet')
+
+  try:
+    while True:
+      schedule.run_pending()
+      time.sleep(1)
+
+  except Exception as e:
+    print e
+    return False
+
+
+if __name__ == '__main__':
+  Main()
